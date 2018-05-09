6b5a53bc973e21770b0fcaf5f54fd372aa619f31
Antonio Kristicevic <kristicevic.antonio@gmail.com>
2018-05-09 13:04:50 +0200

    Add files via upload
    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Qp.Api.Models;
+using System.Reflection;
+
+namespace ResourceChecker
+{
+    class Program
+    {
+        public static void PrintPropertiesToFile(string FileNameContains){
+            var assembly = typeof(Qp.Api.Models.QpApiModelsAssemblyMarker).Assembly;
+            IEnumerable<Type> types = null;
+            HashSet<string> PropertyNames=new HashSet<string>();
+            types = assembly.GetTypes().Where(x => x.Name.Contains("Request"));
+            foreach (var x in types)
+            {
+                foreach (var y in x.GetProperties())
+                {
+                    PropertyNames.Add(y.Name);
+                    Console.WriteLine(x);
+                }
+            }
+            System.IO.File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+@"Write.txt", PropertyNames);
+        }
+    }
+}
